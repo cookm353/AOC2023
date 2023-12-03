7a1c1cce63f47f39de6db1b4ec1410bed1767037
Matthew Cook <cookm353@gmail.com>
2023-12-03 15:17:28 -0500

    day 2 part 1 done

--- Day2/day2.go
+++ Day2/day2.go
@@ -10,32 +10,22 @@ import (
 
 func Part1() {
 	sumValidGameID := 0
-	input := []string{"Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
-		"Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
-		"Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
-		"Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
-		"Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"}
-	// input := readInput("Day2/input1.txt")
+	input := readInput("Day2/input1.txt")
 
 	reBlue := regexp.MustCompile(`[0-9]+.blue`)
 	reRed := regexp.MustCompile(`[0-9]+.red`)
 	reGreen := regexp.MustCompile(`[0-9]+.green`)
 
 	for idx, line := range input {
-		game := []byte(line)
-
-		numRedCubes := breakUpGames(reRed.FindAll(game, -1))
-		numBlueCubes := breakUpGames(reBlue.FindAll(game, -1))
-		numGreenCubes := breakUpGames(reGreen.FindAll(game, -1))
-		// numRedCubes := breakUpGames(reRed.FindAll(line, -1))
-		// numBlueCubes := breakUpGames(reBlue.FindAll(line, -1))
-		// numGreenCubes := breakUpGames(reGreen.FindAll(line, -1))
+		numRedCubes := breakUpGames(reRed.FindAll(line, -1))
+		numBlueCubes := breakUpGames(reBlue.FindAll(line, -1))
+		numGreenCubes := breakUpGames(reGreen.FindAll(line, -1))
 
 		if numRedCubes <= 12 && numBlueCubes <= 14 && numGreenCubes <= 13 {
 			sumValidGameID += idx + 1
 		}
 	}
-	fmt.Println(sumValidGameID)
+	fmt.Println("Sum of game IDs;", sumValidGameID)
 }
 
 /* Read input from file and split into 2-D byte array */
@@ -60,13 +50,15 @@ func readInput(path string) [][]byte {
 /* Return number of cubes played for a given color */
 func breakUpGames(games [][]byte) int {
 	reNumGames := regexp.MustCompile(`[0-9]+`)
-	cubeCount := 0
+	maxCubes := 0
 
 	for _, game := range games {
 		numString := string(reNumGames.Find(game))
 		numCubes, _ := strconv.Atoi(numString)
-		cubeCount += numCubes
+		if numCubes > maxCubes {
+			maxCubes = numCubes
+		}
 	}
 
-	return cubeCount
+	return maxCubes
 }
--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"strings"
 
 	day1 "github.com/cookm353/AdventOfCode/2023/Day1"
 	day2 "github.com/cookm353/AdventOfCode/2023/Day2"
@@ -13,22 +12,14 @@ func main() {
 	for i := 0; i < 10; i++ {
 		// sumCalibrationValues := day1.FindCalibrationValue()
 		sumCalibrationValues := day1.Part1()
-		fmt.Println(sumCalibrationValues)
+		fmt.Println("Sum of calibration values:", sumCalibrationValues)
 	}
-
-	line := "eightwoneight"
-
-	for k, v := range day1.DigitMap {
-		if strings.Contains(line, k) {
-			line = strings.ReplaceAll(line, k, v)
-		}
-	}
-	fmt.Println(line)
 
 	// game := "Game 23: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue"
 
 	// re := regexp.MustCompile(`[0-9]+:`)
 	// gameNumber := string(re.Find([]byte(game)))
 	// fmt.Println(gameNumber[:len(gameNumber)-1])
+	fmt.Println()
 	day2.Part1()
 }
